882baf708994774ac9d2cfb058bc16feee59bce7
kibambe <69826863+kibambe-0167@users.noreply.github.com>
2022-07-07 15:46:34 +0200

    new app, old app not working

--- tidalapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-# get models
-from tidalapp.models import CustomerRegister, UserAdminData
-
-# Register your models here.
-admin.site.register(CustomerRegister)
-admin.site.register(UserAdminData)
